f58840209454eb4660ad506b5b7e264035d014bb
begineer-ops <ayeshanoor131216@gmail.com>
2026-06-02 09:11:44 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
-/*!
- * toidentifier
- * Copyright(c) 2016 Douglas Christopher Wilson
- * MIT Licensed
- */
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+"use strict";
 
-'use strict'
-
-/**
- * Module exports.
- * @public
- */
-
-module.exports = toIdentifier
-
-/**
- * Trasform the given string into a JavaScript identifier
- *
- * @param {string} str
- * @returns {string}
- * @public
- */
-
-function toIdentifier (str) {
-  return str
-    .split(' ')
-    .map(function (token) {
-      return token.slice(0, 1).toUpperCase() + token.slice(1)
-    })
-    .join('')
-    .replace(/[^ _0-9a-z]/gi, '')
-}
+module.exports.AsyncParallelBailHook = require("./AsyncParallelBailHook");
+module.exports.AsyncParallelHook = require("./AsyncParallelHook");
+module.exports.AsyncSeriesBailHook = require("./AsyncSeriesBailHook");
+module.exports.AsyncSeriesHook = require("./AsyncSeriesHook");
+module.exports.AsyncSeriesLoopHook = require("./AsyncSeriesLoopHook");
+module.exports.AsyncSeriesWaterfallHook = require("./AsyncSeriesWaterfallHook");
+module.exports.HookMap = require("./HookMap");
+module.exports.MultiHook = require("./MultiHook");
+module.exports.SyncBailHook = require("./SyncBailHook");
+module.exports.SyncHook = require("./SyncHook");
+module.exports.SyncLoopHook = require("./SyncLoopHook");
+module.exports.SyncWaterfallHook = require("./SyncWaterfallHook");
+module.exports.__esModule = true;
